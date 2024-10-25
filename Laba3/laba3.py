03dfa4e169bdf7154c1ffd6f53dc3c76ceaf862b
Ярослав Пруцков <Roket-liger@yandex.ru>
2024-10-25 11:06:27 +0400

    Laba3 ver2

--- Laba3/laba3.py
+++ Laba3/laba3.py
@@ -11,17 +11,12 @@ def print_chislo(chislo):
         print(i)
     print("===================")
 check=1024
-min_len=''
 s=[]
-flag1,flag2=False,False
 f=open("test.txt","r")
 while True:
     buf = f.readline().split()
     if not buf: break
     for i in range(len(buf)-1):
-        if flag1 and flag2:
-            print_chislo(min_len)
-            flag1,flag2=False,False
         if check2(buf[i]) and check2(buf[i+1]):
             ch1=int(buf[i],2)
             ch2=int(buf[i+1],2)
@@ -29,18 +24,11 @@ while True:
                 if ch1<ch2:
                     if len(s)==0:
                         s.append(buf[i])
-                    s.append(buf[i+1])
-                    min_len=min(s)
-                    flag1=True
-                    if i == len(buf)-2:
-                        print_chislo(min_len)
-                else: 
+                        print_chislo(buf[i])
+                else:
                     s=[]
-                    flag2=True
             else:
                 s=[]
-                flag2=True
         else:
             s=[]
-            flag2=True
 f.close()
